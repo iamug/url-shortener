c3e98353b1233b62694535b75a4a19b3d2e61db7
Aguziendu Ugochukwu <uaguziendu@gmail.com>
2023-04-25 23:30:41 +0100

    implement update method in memory database

--- src/shared/database/memory/memorydb.service.ts
+++ src/shared/database/memory/memorydb.service.ts
@@ -12,6 +12,15 @@ export class MemoryDatabaseService<T> {
     this.data.push(obj);
   }
 
+  updateOneByProperty(query: { prop: keyof T; value: unknown }, data: Partial<T>): T {
+    const { prop, value } = query;
+    const existsIndex = this.data.findIndex((obj) => obj[prop] === value);
+    if (existsIndex < 0) throw new Error('Item does not exist.');
+    const updatedObject = { ...data, [prop]: value };
+    this.data[existsIndex] = { ...this.data[existsIndex], ...updatedObject };
+    return this.data[existsIndex];
+  }
+
   findOneByProperty(prop: keyof T, value: unknown): T {
     return this.data.find((obj) => obj[prop] === value);
   }
