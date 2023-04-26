e05ea045e037877e2116bcfcf9a588679beff4c6
Aguziendu Ugochukwu <uaguziendu@gmail.com>
2023-04-26 09:32:36 +0100

    check for validation error and format response

--- src/shared/filters/http-exception.filter.ts
+++ src/shared/filters/http-exception.filter.ts
@@ -8,12 +8,16 @@ export class HttpExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost): Response<APIErrorResponse> {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response<APIErrorResponse>>();
+
     const exceptionResponse = exception instanceof HttpException ? (exception.getResponse() as any) : null;
     const exceptionStatus = exception instanceof HttpException ? exception.getStatus() : null;
     const status = exceptionResponse?.status || exceptionStatus || HttpStatus.EXPECTATION_FAILED;
+    const isValidationError = status === HttpStatus.UNPROCESSABLE_ENTITY && Array.isArray(exceptionResponse?.message);
+    const message = isValidationError ? 'Validation Error' : exceptionResponse?.message || exception.message || exceptionResponse || 'The process failed';
     return response.status(status).json({
       success: false,
-      message: exceptionResponse?.message || exceptionResponse || exception.message || 'The process failed',
+      message,
+      ...(isValidationError && { error: exceptionResponse?.message }),
       timestamp: new Date().toISOString(),
     });
   }
