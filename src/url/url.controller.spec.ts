6bdd5c91e6c74b255f2cedbf78650db302964e7a
Aguziendu Ugochukwu <uaguziendu@gmail.com>
2023-04-26 08:59:26 +0100

    fix base unit tests for service and controller

--- src/url/url.controller.spec.ts
+++ src/url/url.controller.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UrlController } from './url.controller';
 import { UrlService } from './url.service';
+import { MemoryDatabaseModule } from 'src/shared/database/memory/memorydb.module';
+import { VisitsService } from './visits.service';
 
 describe('UrlController', () => {
   let controller: UrlController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [MemoryDatabaseModule],
       controllers: [UrlController],
-      providers: [UrlService],
+      providers: [UrlService, VisitsService],
     }).compile();
 
     controller = module.get<UrlController>(UrlController);
--- src/url/visits.service.spec.ts
+++ src/url/visits.service.spec.ts
@@ -1,18 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UrlService } from './url.service';
 import { MemoryDatabaseModule } from 'src/shared/database/memory/memorydb.module';
 import { VisitsService } from './visits.service';
 
-describe('UrlService', () => {
-  let service: UrlService;
+describe('VisitsService', () => {
+  let service: VisitsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [MemoryDatabaseModule],
-      providers: [UrlService, VisitsService],
+      providers: [VisitsService],
     }).compile();
 
-    service = module.get<UrlService>(UrlService);
+    service = module.get<VisitsService>(VisitsService);
   });
 
   it('should be defined', () => {
