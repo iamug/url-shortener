fff61db2433177c32a4395cb82c219303ac69bc8
Aguziendu Ugochukwu <uaguziendu@gmail.com>
2023-04-26 08:42:55 +0100

    helper function to group unique values and sort array of objects

--- src/shared/utils/helper.utils.ts
+++ src/shared/utils/helper.utils.ts
@@ -3,3 +3,20 @@ import { urlAlphabet, customAlphabet } from 'nanoid';
 export const randomURLSafeCharacters = (length: number = 10): string => {
   return customAlphabet(urlAlphabet, length)();
 };
+
+export const getUniqueValuesAndCount = <T extends Record<K, any>[], K extends keyof T[number]>(items: T, key: K): { [x in T[number][K]]: number } => {
+  const groupCounts = {} as { [x in T[number][K]]: number };
+  items.forEach((item) => {
+    const value = item[key];
+    if (groupCounts[value]) {
+      groupCounts[value]++;
+    } else {
+      groupCounts[value] = 1;
+    }
+  });
+  return groupCounts;
+};
+
+export const sortByProperty = <T extends Record<string, any>>(arr: T[], prop: keyof T, order: 'ASC' | 'DESC' = 'DESC'): T[] => {
+  return arr.sort((a, b) => (order === 'DESC' ? b[prop] - a[prop] : a[prop] - b[prop]));
+};
